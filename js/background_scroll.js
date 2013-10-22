26a522a4273502e92b9ae1044662d9b32631114a
Sumer Mixon <gitSumer@ximm.us>
2013-10-22 10:20:52 -0400

    array and basic script
    Created the basic function to change background on scroll taking from
    array of images

--- js/background_scroll.js
+++ js/background_scroll.js
@@ -1,3 +1,6 @@
+var bgImages = [ "bg01.jpg", "bg02.jpg", "bg03.jpg", "bg04.jpg", "bg05.jpg", "bg06.jpg", "bg07.jpg", "bg08.jpg", "bg09.jpg", "bg10.jpg", "bg11.jpg" ];
+
 $( window ).scroll(function() {
-	$( "#target" ).css( "background-image", "url(/images/bg02.jpg)" );
+	var bg = bgImages[1];
+	$( "#target" ).css( "background-image", "url(/images/"+ bg +")" );
 });
