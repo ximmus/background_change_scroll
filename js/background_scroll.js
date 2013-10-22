b0a6b303ad52d6f5e3db24a29c5c295870819978
Sumer Mixon <gitSumer@ximm.us>
2013-10-22 11:09:55 -0400

    Cleaned and commented

--- js/background_scroll.js
+++ js/background_scroll.js
@@ -1,15 +1,22 @@
+// array to hold list of background images
 var bgImages = [ "bg01.jpg", "bg02.jpg", "bg03.jpg", "bg04.jpg", "bg05.jpg", "bg06.jpg", "bg07.jpg", "bg08.jpg", "bg09.jpg", "bg10.jpg", "bg11.jpg" ];
+
+// counter initialization
 var i = 0;
+
+// scroll function
 $( window ).scroll(function() {
+	// initalize first background image from array
 	var bg = ( bgImages[ i ] );
 	
+	// traverse the array on each scroll then loop to start of array
 	if( i < bgImages.length ) {
 		i = i + 1;
-		console.log(bgImages.length, i);
 	}
 	if ( i == bgImages.length ) {
 		i = 0;
 	}
 
+	// change the background
 	$( "#target" ).css( "background-image", "url(/images/"+ bg +")" );
 });
